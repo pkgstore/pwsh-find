3a1bc54baef3cbff16abdaa879ad89ccab7c67c6
KitsuneSolar <kitsune.solar@gmail.com>
2023-01-03 00:04:09 +0300

    2023-01-02 21:04:09

--- Find.psm1
+++ Find.psm1
@@ -8,18 +8,12 @@ function Find-CreationTime() {
   [CmdletBinding()]
 
   Param(
-    [Parameter(
-      Mandatory,
-      HelpMessage=""
-    )]
-    [Alias("P")]
+    [Parameter(Mandatory, HelpMessage="")]
+    [Alias("P", "Path")]
     [string]$P_Path,
 
-    [Parameter(
-      Mandatory,
-      HelpMessage="Creation expired time (in seconds). E.g.: '5270400'. Default: 61 day (5270400 sec.)."
-    )]
-    [Alias("T")]
+    [Parameter(Mandatory, HelpMessage="Creation expired time (in seconds). E.g.: '5270400'. Default: 61 day (5270400 sec.).")]
+    [Alias("T", "Time")]
     [long]$P_Time = 5270400
   )
 
@@ -38,18 +32,12 @@ function Find-LastWriteTime() {
   [CmdletBinding()]
 
   Param(
-    [Parameter(
-      Mandatory,
-      HelpMessage=""
-    )]
-    [Alias("P")]
+    [Parameter(Mandatory, HelpMessage="")]
+    [Alias("P", "Path")]
     [string]$P_Path,
 
-    [Parameter(
-      Mandatory,
-      HelpMessage="Last write expired time (in seconds). E.g.: '5270400'. Default: 61 day ('5270400' sec.)."
-    )]
-    [Alias("T")]
+    [Parameter(Mandatory, HelpMessage="Last write expired time (in seconds). E.g.: '5270400'. Default: 61 day ('5270400' sec.).")]
+    [Alias("T", "Time")]
     [long]$P_Time = 5270400
   )
 
